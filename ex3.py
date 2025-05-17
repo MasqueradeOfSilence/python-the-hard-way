7d5d661cda0502375f2c34f66f656d6ae4ca8c76
MasqueradeOfSilence <alexcn711@gmail.com>
2025-05-17 12:05:57 -0700

    commenting

--- ex3.py
+++ ex3.py
@@ -1,23 +1,38 @@
+# I need to comment explaining each line for study drill #1
+# General print statement that explains what we are doing (counting our chickens after they hatch)
 print("I will now count my chickens:")
 
+# Chicken calculation (expected 30)
 print("Hens", 25 + 30 / 6)
+# Rooster calculation (97; modulo is of equal priority to multiplication)
 print("Roosters", 100 - 25 * 3 % 4)
 
+# oh I guess now we're counting our chickens before they hatch :|
 print("Now I will count the eggs:")
 
+# Math
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
+# We ponder the mysteries of the universe
 print("Is it true that 3 + 2 < 5 - 7?")
 
+# Math
 print(3 + 2 < 5 - 7)
 
+# Addition
 print("What is 3 + 2?", 3 + 2)
+# Subtraction
 print("What is 5 - 7?", 5 - 7)
 
+# We have a great realization
 print("Oh, that's why it's False.")
 
+# It's either that slug I ate or I'm having an epiphany
 print("How about some more.")
 
+# The smaller number eats the bigger number
 print("Is it greater?", 5 > -2)
+# More comparators
 print("Is it greater or equal?", 5 >= -2)
+# I love comparators.
 print("Is it less or equal?", 5 <= -2)
