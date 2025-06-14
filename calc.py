4d9312eab1c0534927e82b1c0c72ade28088d674
MasqueradeOfSilence <alexcn711@gmail.com>
2025-06-13 21:42:09 -0500

    ex

---
+++ calc.py
@@ -0,0 +1,4 @@
+flight_time_1_in_minutes = 60
+flight_time_2_in_minutes = 120
+# Expected 180
+print("Total time = " + str(flight_time_1_in_minutes + flight_time_2_in_minutes))
--- ex3.py
+++ ex3.py
@@ -3,26 +3,26 @@
 print("I will now count my chickens:")
 
 # Chicken calculation (expected 30)
-print("Hens", 25 + 30 / 6)
+print("Hens", 25.0 + 30.0 / 6.0)
 # Rooster calculation (97; modulo is of equal priority to multiplication)
-print("Roosters", 100 - 25 * 3 % 4)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
 
 # oh I guess now we're counting our chickens before they hatch :|
 print("Now I will count the eggs:")
 
 # Math
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 
 # We ponder the mysteries of the universe
 print("Is it true that 3 + 2 < 5 - 7?")
 
 # Math
-print(3 + 2 < 5 - 7)
+print(3.0 + 2.0 < 5.0 - 7.0)
 
 # Addition
-print("What is 3 + 2?", 3 + 2)
+print("What is 3 + 2?", 3.0 + 2.0)
 # Subtraction
-print("What is 5 - 7?", 5 - 7)
+print("What is 5 - 7?", 5.0 - 7.0)
 
 # We have a great realization
 print("Oh, that's why it's False.")
@@ -31,8 +31,8 @@ print("Oh, that's why it's False.")
 print("How about some more.")
 
 # The smaller number eats the bigger number
-print("Is it greater?", 5 > -2)
+print("Is it greater?", 5.0 > -2.0)
 # More comparators
-print("Is it greater or equal?", 5 >= -2)
+print("Is it greater or equal?", 5.0 >= -2.0)
 # I love comparators.
-print("Is it less or equal?", 5 <= -2)
+print("Is it less or equal?", 5.0 <= -2.0)
