22471963c9d57e814505f7ef4b68d1ee16e43825
MasqueradeOfSilence <alexcn711@gmail.com>
2026-01-30 19:47:01 -0700

    Study drill ex 2 conversions

--- ex5.py
+++ ex5.py
@@ -16,3 +16,7 @@ print(f"His teeth are usually {teeth} depending on the coffee.")
 # this line is tricky, try to get it exactly right
 total = age + height + weight
 print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+height_in_centimeters = height * 2.54
+weight_in_kilograms = weight * 0.453592
+print(f"Now it's time to be violently non-american: Zed is {height_in_centimeters} cm and {weight_in_kilograms} kgs")
