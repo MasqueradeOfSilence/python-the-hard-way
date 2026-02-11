01bd6e84e6a3979ce55233fa8e06b04ed717fbab
MasqueradeOfSilence <alexcn711@gmail.com>
2026-02-10 20:15:52 -0700

    wip ex6

--- ex6.py
+++ ex6.py
@@ -2,7 +2,7 @@
 types_of_people = 10
 # And this variable prints that info
 x = f"There are {types_of_people} types of people."
-
+# A variable to represent the word binary
 binary = "binary"
 do_not = "don't"
 y = f"Those who know {binary} and those who {do_not}."
