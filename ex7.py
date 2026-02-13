4ceaf450742babc39ad0410dd09f1104ed398628
MasqueradeOfSilence <alexcn711@gmail.com>
2026-02-12 19:38:30 -0700

    that's a lotta comments

---
+++ ex7.py
@@ -0,0 +1,40 @@
+# a print
+print("Mary had a little lamb.")
+# Cool python formatting -- puts snow where those braces would be
+print("Its fleece was white as {}.".format('snow'))
+# a print
+print("And everywhere that Mary went.")
+# prints 10 periods
+print("." * 10) # what'd that do?
+
+# This is a string that reminds me I want a cheeseburger
+end1 = "C"
+# this is also a string, how about double western bacon
+end2 = "h"
+# this is a string
+end3 = "e"
+# it's a string
+end4 = "e"
+# he really wants us to comment every single line? okay, it's a string
+end5 = "s"
+# still a string
+end6 = "e"
+# you guessed it...string
+end7 = "B"
+# string city
+end8 = "u"
+# 10 10 stringies on your stringy strings
+end9 = "r"
+# This is a string that helps form words of cheese and burgers
+end10 = "g"
+# this is a string that makes me think of the haystack tavern double
+end11 = "e"
+# this string says I should go to red robin
+end12 = "r"
+
+# watch end = ' ' at the end. try removing it to see what happens
+# NOTE: When you remove it, it separates the prints by a newline instead of a space
+# and that's what this print does
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# this print is basic just like anyone who thinks shawn mendes is actually good
+print(end7 + end8 + end9 + end10 + end11 + end12)
