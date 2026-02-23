5907224e3d871ff519b8b7febe1be7a457f0a7c8
MasqueradeOfSilence <alexcn711@gmail.com>
2026-02-22 19:27:57 -0700

    Finished ex9 commenting

--- ex9.py
+++ ex9.py
@@ -1,11 +1,17 @@
 # Here's some new strange stuff, remember type it exactly.
 
+# List the days of the week.
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# list the months of the year in this string.
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# print days
 print("Here are the days: ", days)
+# print months
 print("Here are the months: ", months)
 
+# Here we print with newlines! Without having to write \n.
+# Also, we have more freedome with formatting now using this triple-quote setup. 
 print("""
 There's something going on here.
 With the three double-quotes.
