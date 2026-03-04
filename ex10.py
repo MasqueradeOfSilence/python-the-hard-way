4d0e1c6f2777f5a628865e518150fe457c0a5340
MasqueradeOfSilence <alexcn711@gmail.com>
2026-03-04 11:41:14 -0700

    WIP cats

--- ex10.py
+++ ex10.py
@@ -1,6 +1,6 @@
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\\ a \\ cat."
+backslash_cat = "I'm \\ a \\ cat."
 
 fat_cat = """
 I'll do a list:
@@ -8,3 +8,6 @@ I'll do a list:
 \t* Fishies
 \t* Catnip\n\t* Grass
 """
+
+print(tabby_cat)
+print(persian_cat)
