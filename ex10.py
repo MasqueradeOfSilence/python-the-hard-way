24a6c0de1063024877a00c494dc95447278074f7
MasqueradeOfSilence <alexcn711@gmail.com>
2026-03-21 20:21:26 -0700

    rework

--- ex10.py
+++ ex10.py
@@ -2,13 +2,13 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = '''
+fat_cat = """
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-'''
-# using ''' vs """ experiment as instructed by the assignment
+"""
+# The problem with ''' is the "I'll"
 
 print(tabby_cat)
 print(persian_cat)
