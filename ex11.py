d9b638612cf9b2a4ced6e30b237d60a8ebfa1b3e
MasqueradeOfSilence <alexcn711@gmail.com>
2026-04-03 18:17:40 -0700

    input

--- ex11.py
+++ ex11.py
@@ -9,5 +9,6 @@ print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
 '''
 Study Drills:
-1. Python's input 
+1. Python's input method captures input from the user's keyboard. 
+Text parameters passed into it are displayed to the user -- usually a prompt for what text they should enter. 
 '''
