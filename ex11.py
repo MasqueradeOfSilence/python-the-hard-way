450786ba31c8bf5f98ff90147668942b61171ee8
MasqueradeOfSilence <alexcn711@gmail.com>
2026-04-04 19:22:22 -0700

    activities

--- ex11.py
+++ ex11.py
@@ -11,4 +11,6 @@ print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 Study Drills:
 1. Python's input method captures input from the user's keyboard. 
 Text parameters passed into it are displayed to the user -- usually a prompt for what text they should enter. 
+2. I think putting a string as a param to input is the main differentiation here. 
+3. ok fine
 '''
