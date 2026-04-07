2afe21a42c855e364b4c7ea7f4aad990856fb2b7
MasqueradeOfSilence <alexcn711@gmail.com>
2026-04-06 21:12:09 -0700

    CUSTOM FORM

---
+++ ex11_additional_form.py
@@ -0,0 +1,12 @@
+# for ex11: Write another "form" like this to ask some other questions.
+reason_for_ugliness = input("Why are you ugly? ")
+reason_for_being_french = input("Why are you Fr*nch? ")
+dinosaur = input("Why is a dinosaur? ")
+existential = input("What will you do if after death you're still like slightly conscious and then you are lying there "
+                    "in a coffin and then they close it and you can't say anything? ")
+num_ponders = input("How many times this week did you lie awake contemplating the reality of death? ")
+
+print(f"So you are ugly because {reason_for_ugliness}, and you are Fr*nch because {reason_for_being_french}. "
+      f"A dinosaur is because {dinosaur}. If after death you're still like slightly conscious and you are lying "
+      f"there in a coffin and they close it and you can't say anything, you will {existential}. "
+      f"\n\nThis week, you contemplated the reality of death {num_ponders} times.")
