53c20e244d48c3de3021c8e1c44436269921417a
MasqueradeOfSilence <alexcn711@gmail.com>
2026-05-02 21:04:16 -0600

    pydoc

--- ex12.py
+++ ex12.py
@@ -5,4 +5,14 @@ weight = input("How much do you weigh? ")
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
 # Study Drills
-# pydoc command:
+# pydoc command produces the following:
+# QUOTE:
+# Help on built-in function input in module __builtin__:
+#
+# input(...)
+#     input([prompt]) -> value
+#
+#     Equivalent to eval(raw_input(prompt)).
+# END QUOTE
+
+# What does this command do? 
