78724c32a5cd9d38933a641c9904785b8714fb2b
MasqueradeOfSilence <alexcn711@gmail.com>
2026-05-29 19:28:48 -0600

    2 args

--- ex13_script_with_fewer_args.py
+++ ex13_script_with_fewer_args.py
@@ -1,2 +1,6 @@
 from sys import argv
+# Script with fewer arguments and good names for each argument
 script, first, second = argv
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
