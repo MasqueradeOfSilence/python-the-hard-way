fadc9fd28138d96caf990a402aa4d52f02153254
MasqueradeOfSilence <alexcn711@gmail.com>
2026-06-05 20:45:20 +0000

    CAPS

--- ex13_script_with_more_args.py
+++ ex13_script_with_more_args.py
@@ -1,5 +1,5 @@
 from sys import argv
-# example 13 exercise: Script with more than 3 arguments and good names for each argument
+# Example 13 exercise: Script with more than 3 arguments and good names for each argument
 script, first, second, third, fourth = argv
 print("The script is called:", script)
 print("Your first variable is:", first)
