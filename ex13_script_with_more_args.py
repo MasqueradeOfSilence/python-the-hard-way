613ff95a18f9f3f731ae704cfb5edde1eede80bb
MasqueradeOfSilence <alexcn711@gmail.com>
2026-06-06 21:58:27 +0000

    clarity

--- ex13_script_with_more_args.py
+++ ex13_script_with_more_args.py
@@ -1,6 +1,7 @@
 from sys import argv
 # Example 13 exercise: Script with more than 3 arguments and good names for each argument
 script, first, second, third, fourth = argv
+# Note how no space is needed here in the print with comma syntax
 print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
