dd92110f3c28dae4d99ba758fbc350ddc27d626f
MasqueradeOfSilence <alexcn711@gmail.com>
2026-06-09 23:48:30 +0000

    your mom parses argvs

--- ex13_input_with_argv.py
+++ ex13_input_with_argv.py
@@ -1,1 +1,5 @@
 # argv and input to get something from the end user
+import sys
+script_name = sys.argv[0]
+your_mom = input("What is your mom's name? ")
+print(your_mom + " runs the script " + script_name)
