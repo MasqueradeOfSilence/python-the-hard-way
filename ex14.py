2b78a76340a6a2c3fd20cf60e144be79f4172b74
MasqueradeOfSilence <alexcn711@gmail.com>
2026-06-22 20:15:50 -0600

    adding 4

--- ex14.py
+++ ex14.py
@@ -26,6 +26,7 @@ And you have a {computer} computer.  Nice.
 
 # 1. Zork and Adventure are text-based RPGs from the early 80s. Will play at https://classicreload.com/zork-i.html
 # 2. Let's change the prompt to the poop emoji -> 💩 but keep the >
-# 3. Adding another argument: password lmao 
+# 3. Adding another argument: password lmao
+# 4. 
 
 
