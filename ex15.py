6c1efe0f5e3f073ae54f92dbfad9b2cfbb487079
MasqueradeOfSilence <alexcn711@gmail.com>
2026-07-17 00:46:25 +0000

    weird exercise

--- ex15.py
+++ ex15.py
@@ -14,17 +14,18 @@ print(f"Here's your file {filename}:")
 print(txt.read())
 
 # Tell the user to type it again, OR ELSE
-print("Type the filename again:")
+# print("Type the filename again:")
 # The < is displayed to the user as well as a place for them to type
-file_again = input("< ")
+# file_again = input("< ")
 
 # Once again, open the file based on what the user input
-txt_again = open(file_again)
+# txt_again = open(file_again)
 # aaaand print out the contents, fascinating!
-print(txt_again.read())
+# print(txt_again.read())
 
 ## EXERCISES
 # 1, Comments left.
 # 2. need to specify "command" after calling open, so the google ai robot doesn't get confused
 # 3. ok, command/function/method mean the same thing in this context
-# 4. 
+# 4. I commented them in lieu of deleting because then I would delete all my carefully crafted comments, and will run it again 
+# now it doesn't ask for input! wow :O :O :O
