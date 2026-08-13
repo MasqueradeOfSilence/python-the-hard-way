0183579cd7fdc0b98775ed546f043e3689fd3d79
MasqueradeOfSilence <alexcn711@gmail.com>
2026-08-12 20:06:40 -0600

    i promise i'm done with 15 now lol

--- ex15.py
+++ ex15.py
@@ -40,4 +40,5 @@ txt_again.close()
 # 5. To use only input, we would not depend on argv at all, instead passing the value entered in input to the open() command.
 # I did this above. Result: it does, in fact, ask you for input again. wow. exciting
 # 6. python3.6: I have observed that yes, I can do this directly within the python3 console.
-# 7. Uncommenting what was previously commented out. Result: it worked as usual, but cleaned out the streams after for best Python practice.
+# 7. Uncommenting what was previously commented out in order to solve this one.
+#   Result: it worked as usual, but cleaned out the streams after for best Python practice.
