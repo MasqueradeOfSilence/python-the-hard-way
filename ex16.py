72b57a4cea3e93705470aa51ae5a97c5129782bf
MasqueradeOfSilence <alexcn711@gmail.com>
2026-08-18 00:37:21 +0000

    quick ex16 progress

--- ex16.py
+++ ex16.py
@@ -5,3 +5,10 @@ script, filename = argv
 print(f"We're going to erase {filename}.")
 print("If you don't want that, hit CTRL-C (^C).")
 print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+
