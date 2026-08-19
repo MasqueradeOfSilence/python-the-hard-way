f912e874464eea84f641d2ec7c297fd2c5e94156
MasqueradeOfSilence <alexcn711@gmail.com>
2026-08-18 20:06:37 -0600

    wip 16

--- ex16.py
+++ ex16.py
@@ -11,4 +11,11 @@ input("?")
 print("Opening the file...")
 target = open(filename, 'w')
 
+print("Truncating the file. Goodbye!")
+target.truncate()
 
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
