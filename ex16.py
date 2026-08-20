2851d604b63be1151dc2df150a6fe0cda5a8b311
MasqueradeOfSilence <alexcn711@gmail.com>
2026-08-19 20:15:57 -0600

    mini

--- ex16.py
+++ ex16.py
@@ -19,3 +19,5 @@ print("Now I'm going to ask you for three lines.")
 line1 = input("line 1: ")
 line2 = input("line 2: ")
 line3 = input("line 3: ")
+
+print("I'm going to write these to the file.")
