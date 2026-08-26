1301bd1d5952925e313ed7715e7489d7338a926d
MasqueradeOfSilence <alexcn711@gmail.com>
2026-08-26 00:38:06 +0000

    ex16 exercise 2 done

--- ex16_exercise_2_read_and_argv.py
+++ ex16_exercise_2_read_and_argv.py
@@ -5,3 +5,10 @@ script, filename = argv
 target = open(filename, 'r')
 
 # next: read
+# Write a script similar to the last exercise that uses read and argv to read the file you just created.
+
+content = target.read()
+print("Fine, here's your file: ")
+print(content)
+
+target.close()
