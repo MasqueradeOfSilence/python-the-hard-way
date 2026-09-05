1b60039bff3dcf7a4a09f1b7bcc851572f1d3439
MasqueradeOfSilence <alexcn711@gmail.com>
2026-09-04 21:14:29 -0600

    WIP

--- ex17.py
+++ ex17.py
@@ -8,3 +8,5 @@ print(f"Copying from {from_file} to {to_file}")
 # we could do these two on one line, how?
 in_file = open(from_file)
 indata = in_file.read()
+
+print(f"The input file is {len(indata)} bytes long")
