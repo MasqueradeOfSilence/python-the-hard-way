b3b5988d3a7451c77d46516560bc708ebaf278d0
MasqueradeOfSilence <alexcn711@gmail.com>
2026-09-12 22:03:37 -0600

    wip

--- ex17.py
+++ ex17.py
@@ -24,5 +24,7 @@ in_file.close()
 # Results from running: File is copied successfully!
 
 # Exercises:
-# 1. Remove features? okay lol. We will remove the data prompt and some fluff
+# 1. Remove features? okay lol. We will remove the data prompt and some fluff. Done...so it's not
+#   going to match the OG exactly anymore
 # 2. I am sure I could make it 1 line long as well. (TODO)
+#
