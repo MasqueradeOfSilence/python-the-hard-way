773ca8d15de6611eab62411bf1cf6d4b5ae6ad9d
MasqueradeOfSilence <alexcn711@gmail.com>
2026-09-16 00:31:11 +0000

    ex17 wip

--- ex17.py
+++ ex17.py
@@ -27,7 +27,7 @@ in_file.close()
 # 1. Remove features? okay lol. We will remove the data prompt and some fluff. Done...so it's not
 #   going to match the OG exactly anymore
 # 2. I am sure I could make it 1 line long as well. (TODO)
-#   Line: 
+#   Line: I am going to put this in the file `ex17_oneliner.py`. 
 
 
 
