88afb6a9b7476362395d5e8bc2b3fa802b9548f2
MasqueradeOfSilence <alexcn711@gmail.com>
2026-09-21 18:00:24 -0600

    oneliner

--- ex17_oneliner.py
+++ ex17_oneliner.py
@@ -10,8 +10,7 @@ print(f"Copying from {from_file} to {to_file}")
 in_file = open(from_file)
 indata = in_file.read()
 
-print(f"The input file is {len(indata)} bytes long")
-print(f"Does the output file exist? {exists(to_file)}")
+print(f"The input file is {len(indata)} bytes long\nDoes the output file exist? {exists(to_file)}")
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
