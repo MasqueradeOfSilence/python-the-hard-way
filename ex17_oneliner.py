4ac5605605fc401635b6dcd2387aab46a5d983a0
MasqueradeOfSilence <alexcn711@gmail.com>
2026-09-23 00:39:16 +0000

    tentative

--- ex17_oneliner.py
+++ ex17_oneliner.py
@@ -7,8 +7,8 @@ print(f"Copying from {from_file} to {to_file}")
 
 # we could do these two on one line, how?
 # answer: we could do indata = open(from_file).read(), but then we couldn't do a clean close.
-in_file = open(from_file)
-indata = in_file.read()
+# in_file = open(from_file) # TODO won't commenting this out cause issues with closing?
+indata = open(from_file).read()
 
 print(f"The input file is {len(indata)} bytes long\nDoes the output file exist? {exists(to_file)}")
 
@@ -16,7 +16,7 @@ out_file = open(to_file, 'w')
 out_file.write(indata)
 
 out_file.close()
-in_file.close()
+#in_file.close()
 
 # NOTE: Converting this to one line may be difficult with imports. What did he mean by this?
 # semicolons to separate the lines?
